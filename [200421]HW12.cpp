fc5ef41afe6b7e2496c39114789f9f1e40efe622
vincent <vt167098@hotmail.com>
2020-05-06 23:47:18 +0800

    commit 20200506

--- [200421]HW12.cpp
+++ [200421]HW12.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-#include <stdlib.h> 
 
 using namespace std;
 long double x0, y0;
 
 void runSquare(long double x, long double y, long double w, long double n){
 	//cout<<endl<<n<<" "<<w<<" "<<s0.x0<<" "<<s0.y0<<" "<<x<<" "<<y;
-	if (n<0) return;
 	if (x>0&&y>0){
 		x0+=x;
 		y0+=y;
 	}
+	if (n<0) return;
 	runSquare(x+w,y+w,w/2,n-1);
 	runSquare(x+w,y-w,w/2,n-1);
 	runSquare(x-w,y-w,w/2,n-1);
@@ -21,15 +20,15 @@ void runSquare(long double x, long double y, long double w, long double n){
 int main(){
     long double x,y,w,n;
 
-    cin>>x>>y>>w>>n;
-    x0=0;
-    y0=0;
-    //while(n>0){
-	    runSquare(x, y, w/2, n);
-	    //n--; 
-	//}
-	//cout<<endl;
-    cout<<x0<<" "<<y0<<endl;
-
+    while (cin>>x>>y>>w>>n){
+		x0=0;
+		y0=0;
+		//while(n>0){
+			runSquare(x, y, w/2, n);
+			//n--; 
+		//}
+		//cout<<endl;
+		cout<<x0<<" "<<y0<<endl;
+	}
     return 0;
 }
