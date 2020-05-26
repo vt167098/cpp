9f8b54945b9bf8fb880c6a39031f924cafecd617
vincent <vt167098@hotmail.com>
2020-05-26 23:23:52 +0800

    commit 2020052601

--- [200519]HW16.cpp
+++ [200519]HW16.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <string>
 
 using namespace std;
 
@@ -8,6 +9,7 @@ struct Student
 {
     string id;
     string teachers[10];
+    string match;
 };
 
 struct Teacher
@@ -40,6 +42,7 @@ vector<string> string_split(string s, const char delimiter)
 int main() {
     map<int, Student> s_data;
     map<int, Teacher> t_data;
+    map<int, Teacher>::iterator it;
 
     for (int i=0; i<10; i++){
         string s;
@@ -67,5 +70,32 @@ int main() {
         t_data.insert(pair<int, Teacher>(i+1, s_list));
     }
 
+    while (t_data.size()>0){
+        for (int i=1; i<=t_data.size(); i++){
+            Teacher t_temp = t_data[i];
+            vector<int> m_list;
+            for (int j=1; j<=s_data.size(); j++){
+                Student s_temp = s_data[j];
+                if (s_temp.teachers[i-1]==to_string(i)){
+                    s_temp.match = to_string(i);
+                    s_data[j]=s_temp;
+                    m_list.push_back(j);
+                }
+            }
+            if (m_list.size()==1){
+                it = t_data.find(i);
+                if (it != t_data.end())
+                    t_data.erase(it);
+            } else if (m_list.size()>1){
+                for (int k=0; k<10; k++){
+                    for (vector<int>::iterator it1=m_list.begin(); it1!=m_list.end(); it++){
+                        if (t_temp.students[k]==to_string(*it1)){
+
+                        }
+                    }
+                }
+            }
+        }
+    }
 	return 0;
 }
