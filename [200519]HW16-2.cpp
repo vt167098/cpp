db83c0c6b7d676909bb97e5e98d5ecaf4c4e212e
vincent <vt167098@hotmail.com>
2020-06-02 00:03:27 +0800

    commit 20200601

---
+++ [200519]HW16-2.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+struct Student
+{
+    int teachers[10];
+    int match;
+};
+
+struct Teacher
+{
+    int students[10];
+};
+
+vector<string> string_split(string s, const char delimiter)
+{
+    size_t start=0;
+    size_t end=s.find_first_of(delimiter);
+    
+    vector<string> output;
+    
+    while (end <= string::npos)
+    {
+	    output.emplace_back(s.substr(start, end-start));
+
+	    if (end == string::npos)
+	    	break;
+
+    	start=end+1;
+    	end = s.find_first_of(delimiter, start);
+    }
+    
+    return output;
+}
+
+int main() {
+    vector<Student> s_data;
+    vector<Teacher> t_data;
+
+    for (int i=0; i<10; i++){
+        string s;
+        cin>>s;
+        vector<string> ss = string_split(s, '\\,');
+        Student student;
+        for (int j=1; j<=10; j++){
+            student.teachers[j-1] = atoi(ss[j].c_str());
+            student.match = 0;
+        }
+        s_data.push_back(student);
+    }
+
+    for (int i=0; i<10; i++){
+        string s;
+        cin>>s;
+        vector<string> ss = string_split(s, '\\,');
+        Teacher teacher;
+        for (int j=1; j<=10; j++){
+            teacher.students[j-1] = atoi(ss[j].c_str());
+        }
+        t_data.push_back(teacher);
+    }
+
+    while(t_data.size()>0){
+        for (int i=0; i<10; i++){
+            Teacher t = t_data[i];
+            int match = 0;
+            for (int j=0; j<10; j++){
+                if (s_data[j].teachers[i]==i+1){
+                    s_data[j].match = i+1;
+                    if (match==0){
+                        match=i+1;
+                    } else {
+
+                    }
+                }
+            }
+            if (match>0)
+                t_data.erase(t_data.begin()+i);
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
--- [200519]HW16.cpp
+++ [200519]HW16.cpp
@@ -40,77 +40,85 @@ vector<string> string_split(string s, const char delimiter)
 }
 
 int main() {
-    map<int, Student> s_data;
-    map<int, Teacher> t_data;
-    map<int, Teacher>::iterator it;
+    map<string, int> student;
+    map<string, int> teacher;
+    map<string, map<string, int> > s_data;
+    vector< map<string, int> > t_data;
+    //map<int, Teacher>::iterator it;
 
     for (int i=0; i<10; i++){
         string s;
         cin>>s;
         vector<string> ss = string_split(s, '\\,');
-        Student t_list;
-        t_list.id = ss[0];
-        for (int j=0; j<10; j++){
-            t_list.teachers[j] = ss[j+1];
+        //Student t_list;
+        for (int j=1; j<=10; j++){
+            teacher[ss[j]] = j;
         }
+        s_data[ss[0]]=teacher;
 
-        s_data.insert(pair<int, Student>(i+1, t_list));
+        //s_data.insert(pair<int, Student>(i+1, t_list));
     }
 
     for (int i=0; i<10; i++){
         string s;
         cin>>s;
         vector<string> ss = string_split(s, '\\,');
-        Teacher s_list;
-        s_list.id = ss[0];
-        for (int j=0; j<10; j++){
-            s_list.students[j] = ss[j+1];
+        //Teacher s_list;
+        for (int j=1; j<=10; j++){
+            student[ss[j]] = j;
         }
+        t_data.push_back(student);
 
-        t_data.insert(pair<int, Teacher>(i+1, s_list));
+        //t_data.insert(pair<int, Teacher>(i+1, s_list));
     }
 
-    while (t_data.size()>0){
-        for (int i=1; i<=10; i++){
-            it = t_data.find(i);
-            if (it == t_data.end())
-                continue;
 
-            Teacher t_temp = t_data[i];
+//    while (t_data.size()>0){
+        for (int i=0; i<t_data.size(); i++){
+            //it = t_data.find(i);
+            //if (it == t_data.end())
+            //    continue;
+            map<string, int> s_temp = t_data[i]; //教授的學生排行
+
+            //student= t_data[i];
             int pos=11;
             for (int j=1; j<=s_data.size(); j++){
-                Student s_temp = s_data[j];
-                if (s_temp.match!="")
-                    continue;
-                if (s_temp.teachers[i-1]==to_string(i)){
-                    s_temp.match = to_string(i);
-                    s_data[j]=s_temp;
-                    for (int k=0;k<10;k++){
-                        if (t_temp.students[k]==to_string(j)){
-                            if (pos>k+1){
-                                if (k<10&&pos<11){
-                                    int r = stoi(t_temp.students[pos-1]);
-                                    Student s_temp2 = s_data[r];
-                                    s_temp2.match = "";
-                                    s_data[r] = s_temp2;
-                                }
-                                pos = k+1;
-                            } else {
-                                int r = stoi(t_temp.students[k]);
-                                Student s_temp2 = s_data[r];
-                                s_temp2.match = "";
-                                s_data[r] = s_temp2;
-                            }
-                        }
-                    }
-                }
+                map<string, int> t_temp = s_data["A"+to_string(j)];  //學生的教授排行
+                
+                cout<<t_temp[to_string(j)];
+                cout<<endl;
+                // if (s_temp.match!="")
+                //     continue;
+                // if (s_temp.teachers[i-1]==to_string(i)){
+                //     s_temp.match = to_string(i);
+                //     s_data[j]=s_temp;
+                //     for (int k=0;k<10;k++){
+                //         if (t_temp.students[k]==to_string(j)){
+                //             if (pos>k+1){
+                //                 if (k<10&&pos<11){
+                //                     int r = stoi(t_temp.students[pos-1]);
+                //                     Student s_temp2 = s_data[r];
+                //                     s_temp2.match = "";
+                //                     s_data[r] = s_temp2;
+                //                 }
+                //                 pos = k+1;
+                //             } else {
+                //                 int r = stoi(t_temp.students[k]);
+                //                 Student s_temp2 = s_data[r];
+                //                 s_temp2.match = "";
+                //                 s_data[r] = s_temp2;
+                //             }
+                //         }
+                //     }
+                // }
             }
-            if (pos<11){
-                it = t_data.find(i);
-                if (it != t_data.end())
-                    t_data.erase(it);
-            }
+            // if (pos<11){
+            //     it = t_data.find(i);
+            //     if (it != t_data.end())
+            //         t_data.erase(it);
+            // }
         }
-    }
+//    }
+
 	return 0;
 }
